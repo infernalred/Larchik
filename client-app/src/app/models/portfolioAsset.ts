f4ed8d65dfdfbbd74422662c324228b07896a0d7
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-08-22 16:08:05 +0300

    change nginx, docker conf

--- client-app/src/app/models/portfolioAsset.ts
+++ client-app/src/app/models/portfolioAsset.ts
@@ -2,13 +2,7 @@ import { Stock } from "./stock";
 
 export interface PortfolioAsset {
     stock: Stock;
-    // ticker: string;
-    // companyName: string;
-    // sector: string;
-    // type: string;
-    rate: number;
     quantity: number;
-    //price: number;
     amountMarket: number;
     amountMarketCurrency: number;
     averagePrice: number;
