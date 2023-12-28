d19551fe7b0f7033ee82611a0dc417ef5750a051
Alex <39046593+infernalred@users.noreply.github.com>
2023-12-28 19:15:13 +0300

    update packages and create new client app (#13)

--- Larchik.Application/Reports/CurrencyDealsReport.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Larchik.Application.Reports;
-
-public class CurrencyDealsReport
-{
-    public string Account { get; set; } = null!;
-    public string Currency { get; set; } = null!;
-    public decimal Amount { get; set; }
-}
--- Larchik.Application/Reports/ReportResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Larchik.Application.Reports;
-
-public class ReportResult
-{
-    public string FileName { get; set; } = null!;
-    public string MimeType { get; set; } = null!;
-    public byte[] FileData { get; set; } = null!;
-}
