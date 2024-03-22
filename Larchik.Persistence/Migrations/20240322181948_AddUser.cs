242310e21cfce55d1eaa61a34b973b558ef760a8
Alex <39046593+infernalred@users.noreply.github.com>
2024-03-22 21:23:06 +0300

    add users, roles

---
+++ Larchik.Persistence/Migrations/20240322181948_AddUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Larchik.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { new Guid("e5165cd8-4c41-4cc2-8aad-47b879f9da38"), null, "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("7e89d7d2-21e2-40ce-bef2-58c3b9408abb"), 0, "83134201-abcc-49ec-8792-bede7a8e8251", "admin@test.com", true, false, null, "ADMIN@TEST.COM", "ADMIN", "AQAAAAIAAYagAAAAEMAuwtaiWKuKak7jCU+CzruErW/+KfSy8xwPyco2Eq26q0aNwPza/6Ki8rdCQv4DXw==", null, false, null, false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { new Guid("e5165cd8-4c41-4cc2-8aad-47b879f9da38"), new Guid("7e89d7d2-21e2-40ce-bef2-58c3b9408abb") });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("e5165cd8-4c41-4cc2-8aad-47b879f9da38"), new Guid("7e89d7d2-21e2-40ce-bef2-58c3b9408abb") });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("e5165cd8-4c41-4cc2-8aad-47b879f9da38"));
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("7e89d7d2-21e2-40ce-bef2-58c3b9408abb"));
+        }
+    }
+}
