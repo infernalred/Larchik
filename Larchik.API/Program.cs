d61b56d3f767b167fb79ee59c23dbb72d133c48a
Alex <39046593+infernalred@users.noreply.github.com>
2026-01-25 14:53:39 +0300

    serilog

--- Larchik.API/Program.cs
+++ Larchik.API/Program.cs
@@ -1,73 +1,98 @@
 using Larchik.API.Extensions;
-using Larchik.API.Services;
 using Larchik.API.Middleware;
+using Larchik.API.Services;
 using Larchik.Persistence.Context;
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSwaggerServices();
-builder.Services.AddSecurityServices(builder.Configuration);
-builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddCorsServices(builder.Configuration);
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    options.HttpOnly = HttpOnlyPolicy.Always;
-    options.Secure = CookieSecurePolicy.Always;
-    options.MinimumSameSitePolicy = SameSiteMode.Lax;
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddHttpClient();
-
-var app = builder.Build();
-
-using var scope = app.Services.CreateScope();
-
-var services = scope.ServiceProvider;
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
 try
 {
-    var context = services.GetRequiredService<LarchikContext>();
-    await context.Database.MigrateAsync();
-    await IdentitySeeder.SeedAsync(services, builder.Configuration);
-}
-catch (Exception e)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(e, "An error occurred during migration or seed");
-}
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseMiddleware<ExceptionMiddleware>();
+    builder.Host.UseSerilog((context, services, configuration) =>
+        configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext());
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseOpenApi();
-    app.UseSwaggerUi(c => { c.DocumentPath = "/swagger/v1/swagger.json"; });
-}
-app.UseHttpsRedirection();
+    builder.Logging.ClearProviders();
 
-app.UseCors("CorsPolicy");
-app.UseCookiePolicy();
-
-app.UseAuthentication();
-app.Use(async (context, next) =>
-{
-    if (HttpMethods.IsPost(context.Request.Method) ||
-        HttpMethods.IsPut(context.Request.Method) ||
-        HttpMethods.IsDelete(context.Request.Method) ||
-        HttpMethods.IsPatch(context.Request.Method))
+    builder.Services.AddSwaggerServices();
+    builder.Services.AddSecurityServices(builder.Configuration);
+    builder.Services.AddApplicationServices(builder.Configuration);
+    builder.Services.AddCorsServices(builder.Configuration);
+    builder.Services.Configure<CookiePolicyOptions>(options =>
     {
-        var antiforgery = context.RequestServices.GetRequiredService<IAntiforgery>();
-        await antiforgery.ValidateRequestAsync(context);
+        options.HttpOnly = HttpOnlyPolicy.Always;
+        options.Secure = CookieSecurePolicy.Always;
+        options.MinimumSameSitePolicy = SameSiteMode.Lax;
+    });
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddHttpClient();
+
+    var app = builder.Build();
+
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<LarchikContext>();
+        await context.Database.MigrateAsync();
+        await IdentitySeeder.SeedAsync(services, builder.Configuration);
+    }
+    catch (Exception e)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(e, "An error occurred during migration or seed");
     }
 
-    await next(context);
-});
-app.UseAuthorization();
+    app.UseSerilogRequestLogging();
+    app.UseMiddleware<ExceptionMiddleware>();
 
-app.MapControllers();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseOpenApi();
+        app.UseSwaggerUi(c => { c.DocumentPath = "/swagger/v1/swagger.json"; });
+    }
 
-await app.RunAsync();
+    app.UseHttpsRedirection();
+    app.UseCors("CorsPolicy");
+    app.UseCookiePolicy();
+
+    app.UseAuthentication();
+    app.Use(async (context, next) =>
+    {
+        if (HttpMethods.IsPost(context.Request.Method) ||
+            HttpMethods.IsPut(context.Request.Method) ||
+            HttpMethods.IsDelete(context.Request.Method) ||
+            HttpMethods.IsPatch(context.Request.Method))
+        {
+            var antiforgery = context.RequestServices.GetRequiredService<IAntiforgery>();
+            await antiforgery.ValidateRequestAsync(context);
+        }
+
+        await next(context);
+    });
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
