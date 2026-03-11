478ea9281d56bb33113eeaa25d775c5105726b17
Alex <39046593+infernalred@users.noreply.github.com>
2026-03-11 21:32:12 +0300

    extend price

--- src/Larchik.Persistence/Configuration/OperationModelConfiguration.cs
+++ src/Larchik.Persistence/Configuration/OperationModelConfiguration.cs
@@ -9,7 +9,7 @@ public class OperationModelConfiguration : IEntityTypeConfiguration<Operation>
     public void Configure(EntityTypeBuilder<Operation> builder)
     {
         builder.Property(x => x.CurrencyId).IsRequired().HasMaxLength(3);
-        builder.Property(x => x.Price).HasPrecision(18, 4);
+        builder.Property(x => x.Price).HasPrecision(18, 6);
         builder.Property(x => x.Quantity).HasPrecision(18, 6);
         builder.Property(x => x.Fee).HasPrecision(18, 4);
         builder.Property(x => x.Note).HasMaxLength(500);
